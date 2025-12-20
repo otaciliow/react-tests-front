eb1a165969b050201ee887e03b30fbd1fcd2440c
otaciliow <otacilio.dml@hotmail.com>
2025-12-20 17:43:37 -0300

    feat: add page based on TDD

---
+++ src/Signup/index.tsx
@@ -0,0 +1,26 @@
+import type { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './styles.css';
+
+export function SignUp() {
+    const navigate = useNavigate();
+
+    function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        navigate('/dashboard');
+    }
+
+    return (
+        <div>
+            <h1>Cadastre-se</h1>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Insira seu nome" />
+                <input type="text" placeholder="Insira seu e-mail" />
+                <input type="text" placeholder="Insira sua senha" />
+                <button>Cadastrar</button>
+            </form>
+        </div>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Login } from './Login';
 import { Dashboard } from './Dashboard';
+import { SignUp } from './Signup';
 
 export default function MainRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/signup" element={<SignUp />} />
         </Routes>
     )
 }
