d56dcb84f633b6d15bd2d22d715d1a24072d60d4
otaciliow <otacilio.dml@hotmail.com>
2025-12-20 15:40:51 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import MainRoutes from './routes';
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <MainRoutes />
+    </BrowserRouter>
   )
 }
 
