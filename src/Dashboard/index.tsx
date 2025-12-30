b1f27f7cfac67f758e0c47a3693d942857a01a9d
otaciliow <otacilio.dml@hotmail.com>
2025-12-30 20:17:58 -0300

    feat: add api data fetching and add page's style

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react';
+
+import './styles.css';
+
+type PokemonType = {
+    id: number,
+    name: string,
+    image: string,
+    type: string,
+}
+
 export function Dashboard() {
+
+    const [pokemons, setPokemons] = useState<PokemonType[]>([]);
+
+    useEffect(() => {
+        async function loadData() {
+            const response = await fetch('http://localhost:3000/pokemon');
+
+            const data = await response.json();
+
+            setPokemons(data);
+        }
+
+        loadData();
+    }, [])
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <ul>
+                {pokemons.map((pokemon) => (
+                    <li>
+                        <h2>{pokemon.name}</h2>
+                        <img src={pokemon.image} alt={`imagem ${pokemon.name}`} />
+                        <p>{`Tipo: ${pokemon.type}`}</p>
+                    </li>
+                ) )}
+            </ul>
         </div>
     )
 }
